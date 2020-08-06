c0352f932911560a684d1f01e7f67eb331b5e6a1
“Shrenika123” <shrenika.nataraj@gmail.com>
2020-08-07 00:26:10 +0530

    Initial Commit

---
+++ client/build/precache-manifest.063650a788f6ed512421745c6b04eff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e23c675ecaf64ab3feec5c1cfd282f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451d876486c021471703",
+    "url": "/static/css/2.b685cd2e.chunk.css"
+  },
+  {
+    "revision": "ef062c32c6fc5c0567dc",
+    "url": "/static/css/main.05b4a656.chunk.css"
+  },
+  {
+    "revision": "451d876486c021471703",
+    "url": "/static/js/2.ec7187a3.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.ec7187a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef062c32c6fc5c0567dc",
+    "url": "/static/js/main.0cc088bd.chunk.js"
+  },
+  {
+    "revision": "6c6fba9d7f9451bfee27",
+    "url": "/static/js/runtime-main.211a9a59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
